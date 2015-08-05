2a7863fcbfd80ecff6a21f064890a03ab9505ee6
jannesuo <janne.suomalainen@aalto.fi>
2015-08-05 08:42:05 +0300

    Solution - initial items

---
+++ GrillServer.Console/GrillServerService.cs
@@ -0,0 +1,117 @@
+﻿using System.Configuration;
+using System.IO;
+using GrillSrv.Properties;
+using Topshelf;
+using static Grill.Lib.Helpers.ConsoleWriter;
+
+namespace GrillSrv
+{
+    public class GrillServerService : ServiceControl
+    {
+        private const string Description = "Grill Web Server host service";
+        private const string DisplayName = "Grill Web Server";
+        private const string ServiceName = "Grill_web_server";
+        private const string ConfNameSpecifiedPort = "specifiedPort";
+
+        private readonly int _port;
+        private Grill.Lib.ServerSpace.GrillServer _grillServer;
+
+
+
+        public GrillServerService(int port)
+        {
+            _port = port;
+        }
+
+        public bool Start(HostControl hostControl=null)
+        {
+            if (_grillServer != null)
+                return false;
+
+            _grillServer = new Grill.Lib.ServerSpace.GrillServer(_port);
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl=null)
+        {
+            if (_grillServer == null)
+                return false;
+            
+            _grillServer.Dispose();
+            _grillServer = null;
+            return true;
+        }
+
+
+        /// <summary>
+        /// Service initialization for TopShelf library
+        /// </summary>
+        public static void Run(bool startAutomatically=true)
+        {
+            HostFactory.Run(conf =>
+            {
+                
+                conf.AddCommandLineDefinition("port", value =>
+                {
+                    int definedPort;
+                    
+                    if (int.TryParse(value, out definedPort))
+                    {
+                        var port = definedPort;
+                        var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+                        var specifiedPortConf = config.AppSettings.Settings[ConfNameSpecifiedPort];
+                        if (specifiedPortConf != null)
+                            specifiedPortConf.Value = definedPort.ToString();
+                        else
+                            config.AppSettings.Settings.Add(ConfNameSpecifiedPort, definedPort.ToString());
+
+                        config.Save(ConfigurationSaveMode.Modified, true);
+                        ConfigurationManager.RefreshSection("appSettings");
+                    }
+                       
+                });
+
+                conf.ApplyCommandLine();
+                conf.Service<GrillServerService>(srv =>
+                {
+                    srv.ConstructUsing(name =>
+                    {
+                        
+                        ImportantInfo("Loading settings from config file...");
+                        var value = ConfigurationManager.AppSettings[ConfNameSpecifiedPort];
+                        int port;
+                        if (value != null)
+                        {
+                            ImportantInfo($"[Settings] Using specified port value '{value}' from settings");
+                            if (!int.TryParse(value, out port))
+                                port = Settings.Default.defaultPort;
+                        }
+                        else
+                        {
+                           
+                            port = Settings.Default.defaultPort;
+                            ImportantInfo("Using defaultPort value from settings: " + port);
+                        }
+
+                        ImportantInfo("[Settings] Settings loaded");
+                        return new GrillServerService(port);
+                    });
+                    srv.WhenStarted((s, hostControl) => s.Start(hostControl));
+                    srv.WhenStopped((s, hostControl) => s.Stop(hostControl));
+
+                });
+
+                conf.RunAsNetworkService();
+                conf.SetDescription(Description);
+                conf.SetDisplayName(DisplayName);
+                conf.SetServiceName(ServiceName);
+                if (startAutomatically)
+                    conf.StartAutomatically();
+                else
+                    conf.StartManually();
+                
+            });
+        }
+    }
+}
---
+++ GrillServer.Console/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Grill.Lib.ServerSpace;
+using static Grill.Lib.Helpers.ConsoleParameters;
+using static Grill.Lib.Helpers.ConsoleWriter;
+
+namespace GrillSrv
+{
+    internal class Program
+    {
+        const string StartMsg = "[Started] Grill web server started";
+        const string StopMsg = "[Stopped] Grill web server stopped";
+
+
+        private static void Main(string[] args)
+        {
+            
+            ImportantInfo("=== Grill web server ===");
+
+            if (HasHelpSynonym(args))
+            {
+                Warn($"Did you mean \"{GetDefaultHelpParameter}\"?");
+                return;
+            }
+            if (HasPortSynonym(args))
+            {
+                Warn($"Did you mean \"{GetDefaultPortParameter}\"?");
+                return;
+            }
+
+            if (HasHelpParameter(args))
+            {
+                
+                PrintHelpHeader();
+                GrillServerService.Run();
+                return;
+            }
+
+            var serviceMode = HasServiceStartVerb(args);
+            string startInfo = string.Empty;
+            if (serviceMode)
+            {
+                startInfo = "[INFO] Executing Grill server as Windows service";
+                ImportantInfo(StartMsg);
+            }
+               
+
+            else if (!HasServiceVerbs(args))
+            {
+                ImportantInfo(StartMsg);
+                startInfo = "[INFO] Executing Grill server as console application";
+            }
+                
+            else
+                startInfo = "[INFO] Executing Grill service configurations";
+            
+
+           
+            // Run as service or console
+            ImportantInfo(startInfo);
+            GrillServerService.Run();
+            if (!serviceMode)
+                ImportantInfo(StopMsg);
+
+        }
+    }
+}
